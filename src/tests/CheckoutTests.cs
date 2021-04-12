588f8c953540c4b893ab8d54e9ca2b5fd687b6e2
mcinnes01 <mcinnes01@hotmail.com>
2021-04-12 20:30:26 +0100

    Add failing tests

---
+++ src/tests/CheckoutTests.cs
@@ -0,0 +1,43 @@
+﻿using Checkout.Contracts;
+using Checkout.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Checkout.Tests
+{
+    public class CheckoutTests
+    {
+        private readonly ICheckout _checkout;
+
+        public CheckoutTests()
+        {
+            _checkout = new CheckoutService(new List<Product>
+            {
+                new Product { Sku = "A99", UnitPrice = 0.5M },
+                new Product { Sku = "B15", UnitPrice = 0.3M },
+                new Product { Sku = "C40", UnitPrice = 0.6M }
+            });
+        }
+
+        [Fact]
+        public void WhenNoItemsTotalIsZero()
+        {
+            Assert.Equal(0, _checkout.GetTotal());
+        }
+
+        [Theory]
+        [InlineData("A99", 1, 0.5)]
+        [InlineData("A99", 2, 1.0)]
+        [InlineData("B15", 1, 0.3)]
+        [InlineData("C40", 1, 0.6)]
+        [InlineData("C40", 2, 1.2)]
+        [InlineData("C40", 3, 1.8)]
+        [InlineData("C40", 4, 2.4)]
+        [InlineData("C40", 5, 3.0)]
+        public void RequestTotalAndReturnTotal(string sku, int quantity, decimal expected)
+        {
+            _checkout.Scan(sku, quantity);
+            Assert.Equal(expected, _checkout.GetTotal());
+        }
+    }
+}
